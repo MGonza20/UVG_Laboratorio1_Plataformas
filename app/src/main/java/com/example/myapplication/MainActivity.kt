78872ef60d0523d81a308a2c17c78c0792e76fd6
MGonza20 <pag20634@uvg.edu.gt>
2021-04-11 22:46:52 -0600

    Cambié el texto en la rama master

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.toolbar))
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Cambiando", Snackbar.LENGTH_LONG)
+            Snackbar.make(view, "Master", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
     }
